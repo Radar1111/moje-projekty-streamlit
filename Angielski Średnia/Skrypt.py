6f98a724efbfd68e549a95e841f1de8b3e224181
Radar1111 <radek.sz.80@gmail.com>
2026-09-05 13:48:55 +0200

    Update Skrypt.py

--- Angielski Średnia/Skrypt.py
+++ Angielski Średnia/Skrypt.py
@@ -1,7 +1,5 @@
 import streamlit as st
-import pandas as pd
-import requests
-import io
+from datasets import load_dataset
 import random
 
 st.set_page_config(page_title="Apka do Słówek", page_icon="✏️")
@@ -9,14 +7,23 @@ st.title("Moja Nauka Słówek")
 
 # 1. MAPOWANIE NAZW ROZDZIAŁÓW NA PLIKI CSV
 SŁOWNIK_ROZDZIAŁOW = {
-    "Rozdział 1: Liczebniki (Numerals)": "ang_sr_rozdzial1.csv",
-    "Rozdział 2: Szkoła (School)": "ang_sr_rozdzial2.csv",
+    "Rozdział 1: Liczebniki (Numerals)": "ang_kl4_rozdzial1.csv",
+    "Rozdział 2: Szkoła (School)": "ang_kl4_rozdzial2.csv",
+    "Rozdział 3: Owoce (Fruits)": "ang_kl4_rozdzial3.csv",
+    "Rozdział 4: Warzywa (Vegetables)": "ang_kl4_rozdzial4.csv",
+    "Rozdział 5: Rodzina (Family)": "ang_kl4_rozdzial5.csv",
+    "Rozdział 6: Zwierzęta (Animals)": "ang_kl4_rozdzial6.csv",
+    "Rozdział 7: Pokoje w domu (Rooms)": "ang_kl4_rozdzial7.csv",
+    "Rozdział 8: Kolory (Colors)": "ang_kl4_rozdzial8.csv",
+    "Rozdział 9: Części ciała (Body parts)": "ang_kl4_rozdzial9.csv",
+    "Rozdział 10: Miejsca w szkole (School places)": "ang_kl4_rozdzial10.csv",
+    "Rozdział 11: Polecenia nauczyciela (Teacher's instruction)": "ang_kl4_rozdzial11.csv",
 }
 
 def laduj_slowka(nazwa_pliku):
     """Pobiera bazę słów z Hugging Face Dataset"""
     try:
-        repo_id = "Radar1111/Angielski-Sr" 
+        repo_id = "Radar1111/angielski-klasa-4" 
         dataset = load_dataset(
             repo_id, 
             data_files=nazwa_pliku, 
@@ -30,24 +37,6 @@ def laduj_slowka(nazwa_pliku):
     except Exception as e:
         st.error(f"Problem z połączeniem lub brakiem pliku: {e}")
         return []
-            
-        df = pd.read_csv(io.StringIO(response.text))
-        
-        pobrane_dane = []
-        for _, wiersz in df.iterrows():
-            pobrane_dane.append({
-                'pl': str(wiersz['pl']).strip(), 
-                'en': str(wiersz['en'].strip()
-            )})
-        return pobrane_dane
-        
-    except Exception as e:
-        st.error(f"Problem z połączeniem lub brakiem pliku: {e}")
-        return []
-
-# ---- DALSZA CZĘŚĆ TWOJEGO KODU ----
-wybrany_opis = st.selectbox("Który rozdział?", list(SŁOWNIK_ROZDZIAŁOW.keys()))
-
 def sprawdz_odpowiedz_wpisywanie():
     """Sprawdza wpisaną odpowiedź w trybie Wpisywania"""
     klucz = f"temp_odp_{st.session_state.index}"
